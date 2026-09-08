e0dcc67f672ef726ab9e71ff915fbc543fc54760
IkoStirling <IkoStirling@gmail.com>
2026-09-09 00:57:20 +0800

    test(ui-designer): cover reusable responsive chrome

--- unittest/Test_EditorShell.cpp
+++ unittest/Test_EditorShell.cpp
@@ -299,6 +299,10 @@ TEST_CASE(ui_layout_editor_is_hosted_as_one_owned_tool_window) {
     CHECK(childUi->findById("style_source_status") != nullptr);
     CHECK(childUi->findById("validation_list") != nullptr);
     CHECK(childUi->findById("btn_validate") != nullptr);
+    CHECK(childUi->findById("reuse_list") != nullptr);
+    CHECK(childUi->findById("btn_reuse_define") != nullptr);
+    CHECK(childUi->findById("responsive_breakpoint") != nullptr);
+    CHECK(childUi->findById("responsive_visibility") != nullptr);
     CHECK(addButton == nullptr || addButton->getIconDocument() != nullptr);
     CHECK(addLabel == nullptr || addLabel->getIconDocument() != nullptr);
     CHECK(addPanel == nullptr || addPanel->getIconDocument() != nullptr);
